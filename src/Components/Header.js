88e38cb253caf061e2c49f9dd3fc13c10caeed72
Sunwha <ohsw17@naver.com>
2021-01-19 17:59:14 +0900

    add icons

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
 import styled from "styled-components";
+import movieImg from "images/clapperboard.svg";
+import tvImg from "images/tv.svg";
+import searchImg from "images/loupe.svg";
 
 const Header = styled.header`
 position:fixed; top:0; bottom:0; left:0; width:50px; text-align:center; background:rgba(0, 0, 0, .8);
@@ -11,12 +14,24 @@ display:inline-block; position:relative; top:50%; transform:translateY(-50%);
 `;
 
 const Item = styled.li`
-width:36px; height:36px; line-height:36px; font-size:10px; color:#fff; background:rgba(255, 255, 255, .2);
+position:relative; width:36px; height:36px; line-height:36px; font-size:0; color:#fff; border-radius:100%; 
+background:rgba(255, 255, 255, ${props => props.current ? "1" : ".7"});
+&:hover {background:rgba(255, 255, 255, 1)}
+a {
+    &:after {content:""; position:absolute; top:0; right:0; bottom:0; left:0; z-index:0; background-size:22px; background-repeat:no-repeat; background-position:50% 50%}
+}
+&:first-child {
+    a:after {background-image:url(${movieImg})}
+}
+&:nth-child(2) {
+    a:after {background-image:url(${tvImg})}
+}
+&:nth-child(3) {
+    a:after {background-image:url(${searchImg})}
+}
 ~ {
     li {margin-top:50px}
 }
-border-bottom:3px solid ${props => props.current ? "red" : ""};
-&:hover{background:rgba(255, 255, 255, .4)}
 `;
 
 const $Link = styled(Link)`
